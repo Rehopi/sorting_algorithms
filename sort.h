279cd5b6be474876aa40b5c6cd2f7f9f0f0c4da2
rehopi <emekaokechukwu16@gmail.com>
2022-10-09 17:48:38 -0700

    sort.h file

---
+++ sort.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
